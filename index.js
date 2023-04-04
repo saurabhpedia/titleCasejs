80ec97d4f75fb9eba6ad23a99c665ab368b44ab1
lmsworld <--add>
2023-04-04 18:43:55 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function toTitleCase(str) {
+  return str.toLowerCase().split(' ').map(word => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(' ');
+}
+
+module.exports = { toTitleCase };
